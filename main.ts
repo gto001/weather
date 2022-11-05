743ca627aa7e5683c1475981c4f507df81b3b89a
gto001 <90951381+gto001@users.noreply.github.com>
2022-11-05 16:51:33 +0900

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -11,11 +11,11 @@ bluetooth.onUartDataReceived(serial.delimiters(Delimiters.NewLine), function () 
     if (num == 0) {
         pins.servoWritePin(AnalogPin.P0, 0)
     } else if (num == 1) {
-        pins.servoWritePin(AnalogPin.P0, 45)
+        pins.servoWritePin(AnalogPin.P0, 40)
     } else if (num == 2) {
-        pins.servoWritePin(AnalogPin.P0, 90)
+        pins.servoWritePin(AnalogPin.P0, 85)
     } else if (num == 3) {
-        pins.servoWritePin(AnalogPin.P0, 135)
+        pins.servoWritePin(AnalogPin.P0, 115)
     } else if (num == 4) {
         pins.servoWritePin(AnalogPin.P0, 175)
     }
